20d1acba4a81e6bfc9c4750e8c878da703e012d4
Bnaya Peretz <me@bnaya.net>
2017-03-03 23:49:04 +0200

    Why error???

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,27 @@
-import "source-map-support/register";
-import * as aws from "aws-sdk";
-import { S3 } from "aws-sdk";
-import S3BucketAsyncIterator from './S3BucketAsyncIterator';
-import { S3IteratorOptions } from './S3BucketIterable';
+class FooAsyncIterator implements AsyncIterator<string> {
+  next(value?: any) {
+    return Promise.resolve({
+      value: 'foooo',
+      done: true
+    });
 
-
-const bucket = "foo";
-
-
-export default class S3BucketIterable implements AsyncIterable<S3.Types.Object> {
-  constructor(private s3: S3, private options: S3IteratorOptions) {
-  }
-
-  [Symbol.asyncIterator]() {
-    return new S3BucketAsyncIterator(this.s3, this.options);
   }
 }
 
+class FooAsyncIterable implements AsyncIterable<string> {
 
-const s3Client = new aws.S3({
-  credentials: {
-    accessKeyId: "",
-    secretAccessKey: ""
+  [Symbol.asyncIterator]() {
+    return new FooAsyncIterator()
   }
-});
-
+}
 
 const fooBucket2: AsyncIterable<string> = {
   [Symbol.asyncIterator]() {
-    return new S3BucketAsyncIterator(s3Client, {
-      Bucket: bucket,
-      Prefix: 'events/',
-      MaxKeys: 20,
-      // Delimiter: '/'
-    });
+    return new FooAsyncIterator()
   }
 };
 
-const oBucket = new S3BucketIterable(s3Client, {
-  Bucket: bucket,
-  Prefix: 'events/',
-  MaxKeys: 20,
-  // Delimiter: '/'
-});
+const fooAsyncIterable = new FooAsyncIterable();
 
 async function foo() {
 
@@ -51,7 +30,7 @@ async function foo() {
   }
 
   // why error ????
-  for await (const s3obj of oBucket) {
+  for await (const s3obj of fooAsyncIterable) {
 
   }
 }
